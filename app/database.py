45cbae2b6d3d8756a7159cf2b053fa026318cf74
Dhanuka Sheyam Perera <dhanuka.sp@outlook.com>
2024-06-16 01:35:45 +0530

    [bugfix] fixed an issue with database synchronisation and object deserialisation

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
@@ -10,7 +11,8 @@ MONGODB_CLUSTER_URL = os.getenv("MONGODB_CLUSTER_URL")
 
 uri = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER_URL}/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = MongoClient(uri)
+# client = MongoClient(uri)
+client = AsyncIOMotorClient(uri)
 
 try:
     client.admin.command("ping")
@@ -18,5 +20,7 @@ try:
 except Exception as e:
     print(e)
 
-database = client.form_database
-form_collection = database.get_collection("form_data")
+# database = client.form_database
+database = client["form_database"]
+# form_collection = database.get_collection("form_data")
+form_collection = database["form_data"]
