c819086146e1ff6da0ee10e8d307010e0e945479
Dhanuka Sheyam Perera <dhanuka.sp@outlook.com>
2024-06-16 00:44:34 +0530

    mongodb connection scripts added

---
+++ app/database.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+
+load_dotenv()
+
+MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
+MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+MONGODB_CLUSTER_URL = os.getenv("MONGODB_CLUSTER_URL")
+
+uri = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER_URL}/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri)
+
+try:
+    client.admin.command("ping")
+    print("Pinged the deployment. Successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+database = client.form_database
+form_collection = database.get_collection("form_data")
