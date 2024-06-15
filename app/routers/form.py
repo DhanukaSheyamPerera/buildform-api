2a0c1085ba0d923154278db7c30ef1a0419d555d
Dhanuka Sheyam Perera <dhanuka.sp@outlook.com>
2024-06-16 01:10:17 +0530

    router added for form

--- app/routers/form.py
+++ app/routers/form.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
+from app.schemas import FormData
+from app.crud import create_form_data
+
 
 router = APIRouter()
 
 @router.post("/submit-form")
-def submit_form():
-    return {"data": "form-submitted"}
+async def submit_form(form_data: FormData):
+    created_data = await create_form_data(form_data)
+    return created_data
